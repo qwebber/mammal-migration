862a6808fecfa186ea30e0efe6c87cb5ca586860
qwebber <webber.quinn@gmail.com>
2019-01-15 09:31:48 -0330

    re-do therm scope analysis

--- scripts/7.0-TerrThermScopeAnalysis.R
+++ scripts/7.0-TerrThermScopeAnalysis.R
@@ -11,71 +11,43 @@ TSTerr <- readRDS("output/compTSterrData.RDS")
 
 ## model selection
 ## global model
-TS1=pgls(mig~logmass+ TS + habitat_sub+diet+abslat,data=TSTerr,lambda="ML")
-##w/o logmass
-TS2=pgls(mig ~ TS+habitat_sub+diet+abslat,data=TSTerr,lambda="ML")
-##w/o TS
-TS3=pgls(mig ~ logmass+habitat_sub+diet+abslat,data=TSTerr,lambda="ML")
-## w/o habitat
-TS4 = pgls(mig ~ TS+logmass+diet+abslat,data=TSTerr,lambda="ML")
-##w/o abslat
-TS5 = pgls(mig ~ TS+logmass+habitat_sub+diet,data=TSTerr,lambda="ML")
-## w/o diet
-TS6= pgls(mig ~ TS+logmass+habitat_sub+abslat,data=TSTerr,lambda="ML")
+### global model
+TS1=pgls(mig~TS + logmass + diet + abslat + habitat_sub,data=TSTerr,lambda="ML")
 
-AIC(TS1,TS2,TS3,TS4,TS5,TS6)
+TS2 = pgls(mig ~ TS*diet + logmass + abslat,data=TSTerr,lambda="ML")
 
-##w/o habitat and diet
-TS7 = pgls(mig ~ TS+logmass+abslat,data=TSTerr,lambda="ML")
-##w/o habitat and abslat
-TS8 = pgls(mig ~ TS+logmass+diet,data=TSTerr,lambda="ML")
-##w/o habitat and logmass
-TS9 = pgls(mig ~ TS+diet+abslat,data=TSTerr,lambda="ML")
-## w/o habitat and TS
-TS10 =pgls(mig ~ logmass+diet+abslat,data=TSTerr,lambda="ML")
-##w/o diet and abslat
-TS11 = pgls(mig ~ TS+logmass+habitat_sub,data=TSTerr,lambda="ML")
-##w/o diet and logmass
-TS12 = pgls(mig ~ TS+habitat_sub+abslat,data=TSTerr,lambda="ML")
-## w/o diet and TS
-TS13 =pgls(mig ~ logmass+habitat_sub+abslat,data=TSTerr,lambda="ML")
-##w/o abslat and logmass
-TS14 = pgls(mig ~ TS+habitat_sub+diet,data=TSTerr,lambda="ML")
-## w/o abslat and TS
-TS15 =pgls(mig ~ logmass+habitat_sub+diet,data=TSTerr,lambda="ML")
+TS3 = pgls(mig ~ TS*habitat_sub + logmass + abslat,data=TSTerr,lambda="ML")
 
-AIC(TS1,TS2,TS3,TS4,TS5,TS6,TS7,TS8,TS9,TS10,TS11,TS12,TS13,TS14,TS15)
+TS4 = pgls(mig ~ TS*diet + logmass,data=TSTerr,lambda="ML")
 
-TS16 =pgls(mig ~ logmass ,data=TSTerr,lambda="ML")
-TS17 =pgls(mig ~ TS ,data=TSTerr,lambda="ML")
-TS18 =pgls(mig ~ abslat ,data=TSTerr,lambda="ML")
-TS19 =pgls(mig ~ TS*diet ,data=TSTerr,lambda="ML")
-TS20 =pgls(mig ~ habitat_sub ,data=TSTerr,lambda="ML")
+TS5 = pgls(mig ~ TS*diet + latitude,data=TSTerr,lambda="ML")
+
+TS6 = pgls(mig ~ TS*habitat_sub + logmass,data=TSTerr,lambda="ML")
+
+TS7 = pgls(mig ~ TS*habitat_sub + latitude,data=TSTerr,lambda="ML")
+
+TS8 = pgls(mig ~ TS*diet,data=TSTerr,lambda="ML")
+
+TS9 = pgls(mig ~ TS*habitat_sub,data=TSTerrHab,lambda="ML")
 
 
-TSAIC <- data.table(AICc(TS1,TS2,TS3,TS4,TS5,TS6,TS7,TS8,TS9,TS10,TS11,TS12,TS13,TS14,TS15,TS16,TS17,TS18,TS19,TS20),
-                    round(Weights(AIC(TS1,TS2,TS3,TS4,TS5,TS6,TS7,TS8,TS9,TS10,TS11,TS12,TS13,TS14,TS15,TS16,TS17,TS18,TS19,TS20)),3))
+TSAIC <- data.table(AICc(TS1,TS2,TS3,TS4,TS5,TS6,TS7,TS8,TS9),
+                    round(Weights(AIC(TS1,TS2,TS3,TS4,TS5,TS6,TS7,TS8,TS9)),3))
 
 TSAIC$lambda <- rbind(TS1$param[2], TS2$param[2], TS3$param[2], TS4$param[2],
                       TS5$param[2], TS6$param[2], TS7$param[2], TS8$param[2],
-                      TS9$param[2], TS10$param[2], TS11$param[2], TS12$param[2], 
-                      TS13$param[2], TS14$param[2], TS15$param[2], TS16$param[2], 
-                      TS17$param[2], TS18$param[2], TS19$param[2], TS20$param[2])
+                      TS9$param[2])
 
 TSAIC$model <- rbind(as.character(TS1$call[2]),as.character(TS2$call[2]),
                      as.character(TS3$call[2]),as.character(TS4$call[2]),
                      as.character(TS5$call[2]),as.character(TS6$call[2]),
                      as.character(TS7$call[2]),as.character(TS8$call[2]),
-                     as.character(TS9$call[2]),as.character(TS10$call[2]),
-                     as.character(TS11$call[2]),as.character(TS12$call[2]),
-                     as.character(TS13$call[2]),as.character(TS14$call[2]),
-                     as.character(TS15$call[2]),as.character(TS16$call[2]),
-                     as.character(TS17$call[2]),as.character(TS18$call[2]),
-                     as.character(TS19$call[2]),as.character(TS20$call[2]))
+                     as.character(TS9$call[2]))
 
 TSAIC <- TSAIC[order(AICc)]
 
 TSAIC$deltaAIC <- TSAIC$AICc - as.numeric(TSAIC[1,2])
+
 
 
 write.csv(TSAIC, "output/TSTerrAIC.csv")
