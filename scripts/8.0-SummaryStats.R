0db9e5faaddd297ec2a8c93213003ccbab0420b0
Quinn Webber <webber.quinn@gmail.com>
2020-04-17 17:44:37 -0230

    Update 8.0-SummaryStats.R

--- scripts/8.0-SummaryStats.R
+++ scripts/8.0-SummaryStats.R
@@ -17,3 +17,11 @@ Mamm[, .N, by = .(mig, locomotion, diet)]
 (53+12)/68
 ## prop herbivorous migratory running mammals
 46/54
+
+## Therm scope summary: 
+TS = fread("input/thermoregulatory_scope.csv",header = T)
+TS <- merge(Mamm, TS, by = "species")
+
+ggplot(TS[locomotion == "T"]) +
+  geom_point(aes(TS, mig)) +
+  facet_wrap(~diet)
